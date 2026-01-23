0b79fad88bbb00a87ad73ea46ae46091548ccb48
Johnny Strømø <113671758+JohnnyBravo1983@users.noreply.github.com>
2026-01-23 23:16:07 +0100

    vercel: add api probe function

---
+++ frontend/api/_probe.js
@@ -0,0 +1,4 @@
+﻿module.exports = (req, res) => {
+  res.setHeader("content-type", "application/json; charset=utf-8");
+  res.status(200).send(JSON.stringify({ ok: true, from: "vercel-function" }));
+};
